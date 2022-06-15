2d8c0151dc5a26b54625ef8bef4d963a27386d21
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-06-15 09:21:56 -0400

    Update variable name and documentation

--- Gradient.java
+++ Gradient.java
@@ -6,28 +6,32 @@ import java.util.ArrayList;
 
 public class Gradient {
     public static void main(String[] args) throws IOException {
-        ArrayList<ArrayList<double[]>> gradients = new ArrayList<>();
+        ArrayList<ArrayList<double[]>> points = new ArrayList<>();
         ArrayList<double[]> row = new ArrayList<>();
         double x, y, z, last = 0;
 
         try (BufferedReader reader = Files.newBufferedReader(Path.of("data/ncsutest.xyz"))) {
             String line;
             while ((line = reader.readLine()) != null) {
+                // Split line by spaces and read coordinates
                 String[] tokens = line.split("\\s+");
                 x = Double.parseDouble(tokens[0]);
                 y = Double.parseDouble(tokens[1]);
                 z = Double.parseDouble(tokens[2]);
 
+                // If y value has changed, add the entire row to the list and restart
                 if (y != last) {
                     if (row.size() != 0) {
-                        gradients.add(row);
+                        points.add(row);
                     }
                     row = new ArrayList<>();
                 }
                 
+                // Add points
                 row.add(new double[] {x, y, z});
                 last = y;
             }
+            points.add(row);
         }
     }
 }

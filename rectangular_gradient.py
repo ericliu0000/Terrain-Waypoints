3a1e160476919d86df5f088d761aa11e2e6838ce
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-06-15 15:24:34 -0400

    unit normal for rectangular grids

--- rectangular_gradient.py
+++ rectangular_gradient.py
@@ -14,8 +14,18 @@ for i in range(1, len(points) - 1):
         y_deriv = (points[i + 1][j][2] - points[i - 1][j][2]) / (points[i + 1][j][1] - points[i - 1][j][1]) 
 
         # print(x_deriv, y_deriv)
-        print(f"z1 {points[i][j + 1][2]}, z2 {points[i][j - 1][2]}, x1 {points[i][j + 1][0]}, x2 {points[i][j - 1][0]} xderiv {x_deriv}")
+        print(f"z1 {points[i][j + 1][2]} z2 {points[i][j - 1][2]}\nx1 {points[i][j + 1][0]} x2 {points[i][j - 1][0]} xderiv {x_deriv}\ny1 {points[i + 1][j][1]} y2 {points[i - 1][j][1]} yderiv {y_deriv}")
         print(numpy.cross((1, 0, x_deriv), (0, 1, y_deriv)))
 
         # Get normal vector from x and y partial derivative
-        normal = (x_deriv, y_deriv)
+        normal = numpy.cross((1, 0, x_deriv), (0, 1, y_deriv))
+
+        # get unit normal
+        normal = normal / numpy.linalg.norm(normal)
+
+        print(normal)
+        # add to gradients
+        gradients.append(normal)
+
+        print()
+        input()

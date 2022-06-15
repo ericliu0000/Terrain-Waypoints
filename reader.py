fb16186db505d2b839d72d07539772d40fc632f6
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-06-15 14:20:09 -0400

    Rename gradient and add oop

---
+++ reader.py
@@ -0,0 +1,37 @@
+"""
+Read data to latitude, longitude, and elevation
+"""
+import pyproj
+
+
+class XYZReader:
+    points = []
+    transformer = pyproj.Transformer.from_proj("epsg:32119", "epsg:4326")
+
+    def __init__(self, file):
+        with open(file) as f:
+            # Read x, y, z points
+            last = 0
+            row = []
+
+            for line in f.readlines():
+                line = line.replace("\n", "")
+                x, y, z = line.split()
+
+                # If y value has advanced, add row to the list
+                if y != last:
+                    if len(row) != 0:
+                        self.points.append(row)
+
+                    row = []
+
+                # Add values (from survey feet to meters)
+                row.append((float(x) * 1200 / 3937, float(y) * 1200 / 3937, float(z)))
+                last = y
+
+            self.points.append(row)
+
+
+if __name__ == "__main__":
+    reader = XYZReader("data/cloud_simplified.txt")
+    print(reader.points[0])

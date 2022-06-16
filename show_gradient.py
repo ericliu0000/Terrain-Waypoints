4ebf7a97c5ea913c3c00f70bd295feb5e1d7dba5
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-06-16 15:27:40 -0400

    start work on gradient

--- show_gradient.py
+++ show_gradient.py
@@ -3,32 +3,43 @@ import numpy
 from scipy.interpolate import griddata
 import matplotlib.pyplot as plt
 
-# elevation
-elevations = PandasReader("data/ncsutest.h5").points
-X, Y = elevations[..., 0].flatten(), elevations[..., 1].flatten()
-Z = elevations[..., 2]
 
-x_i = numpy.linspace(X.min(), X.max(), 1600)
-y_i = numpy.linspace(Y.min(), Y.max(), 1600)
+def NCSUTest():
+    elevations = PandasReader("data/ncsutest.h5").points
+    X, Y = elevations[..., 0].flatten(), elevations[..., 1].flatten()
+    Z = elevations[..., 2]
 
-z_min = Z.min()
-z_max = Z.max()
+    x_i = numpy.linspace(X.min(), X.max(), 1600)
+    y_i = numpy.linspace(Y.min(), Y.max(), 1600)
 
-plt.subplot(1, 2, 1)
-plt.contourf(x_i, y_i, Z, 160, cmap=plt.cm.terrain, vmin=z_min, vmax=z_max)
-plt.colorbar()
+    z_min = Z.min()
+    z_max = Z.max()
 
-# gradient
-calculator = NumpyGradient()
-gradient = calculator.magnitude
+    plt.subplot(1, 2, 1)
+    plt.contourf(x_i, y_i, Z, 160, cmap=plt.cm.terrain, vmin=z_min, vmax=z_max)
+    plt.colorbar()
 
-z_min = -1
-z_max = 1
+    # gradient
+    calculator = NumpyGradient()
+    gradient = calculator.magnitude
 
-Z = numpy.clip(gradient, z_min, z_max)
+    z_min = -1
+    z_max = 1
 
-plt.subplot(1, 2, 2)
-plt.contourf(x_i, y_i, Z, 80, cmap=plt.cm.coolwarm, vmin=z_min, vmax=z_max)
-plt.colorbar()
+    Z = numpy.clip(gradient, z_min, z_max)
 
-plt.show()
+    plt.subplot(1, 2, 2)
+    plt.contourf(x_i, y_i, Z, 80, cmap=plt.cm.coolwarm, vmin=z_min, vmax=z_max)
+    plt.colorbar()
+
+    plt.show()
+
+def siteTest():
+    obj = InterpolatedGridGradient("data/cloud_simplified.h5")
+    X, Y = obj.spacing[0], obj.spacing[1]
+
+    # TODO complete this. do the same as the above function but just adapt the points to the interpolated ones instead
+
+
+if __name__ == "__main__":
+    NCSUTest()

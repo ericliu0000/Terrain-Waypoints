dc12957278bb6e1beb720443748e8bd1aaa4e648
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-06-24 10:00:18 -0400

    Cleanup code a bit'

--- waypoint.py
+++ waypoint.py
@@ -2,50 +2,48 @@ from filter import SiteFilter
 import matplotlib.pyplot as plt
 import numpy
 
-site = SiteFilter("data/cloud_lasground.h5", [3500])
-values = list(site.coords.values())
-
-for point in values:
-    point = numpy.array(sorted(point, key=lambda x: x[1]))
-
-    # polynomial fit -- testing
-    eq = numpy.polyfit(point[:, 1], point[:, 0], 3)
-    x = numpy.linspace(point[:, 1].min(), point[:, 1].max(), 100)
-
-    y = 0
-    for degree, coefficient in enumerate(eq[::-1]):
-        y += coefficient * x ** degree
-
-    plt.plot(y, x, "b")
-
-    print(x, y)
+class WaypointGenerator:
+    # TODO: make the length work
     length = 150
+    new_points = []
 
-    # Slope perpendicular to the fit
-    for i in range(1, len(x) - 1):
-        last = (x[i - 1], y[i - 1])
-        next = (x[i], y[i])
+    def __init__(self, doc, height):
+        site = SiteFilter(doc, height)
+        values = list(site.coords.values())
 
-        midpoint = ((last[1] + next[1]) / 2, (last[0] + next[0]) / 2)
-        normal = -(next[1] - last[1]) / (next[0] - last[0])
-        unit = (1, normal) / numpy.linalg.norm((1, normal))
-        
-        # print(midpoint, normal)
-        new_point = (midpoint[0] + unit[0] * length, midpoint[1] + unit[1] * length)
-        print(midpoint, new_point)
+        for point in values:
+            point = numpy.array(sorted(point, key=lambda x: x[1]))
+            # plot points
+            plt.plot(point[:, 0], point[:, 1], "go")
 
-        plt.plot([midpoint[0], new_point[0]], [midpoint[1], new_point[1]], "b")
+            # polynomial fit
+            eq = numpy.polyfit(point[:, 1], point[:, 0], 3)
+            x = numpy.linspace(point[:, 1].min(), point[:, 1].max(), 100)
 
-    # Find the slope perpendicular to all the points
-    # slopes = []
+            # plot fit
+            y = 0
+            for degree, coefficient in enumerate(eq[::-1]):
+                y += coefficient * x ** degree
 
-    # for i in range(1, len(point) - 1):
-    #     slopes.append(-(point[i][0] - point[i - 1][0]) / (point[i][1] - point[i - 1][1]))
-    #     plt.plot([point[i - 1][0], point[i][0]], [point[i - 1][1], point[i][1]], "go")
+            plt.plot(y, x, "b")
+
+            # Slope perpendicular to the fit
+            for i in range(1, len(x) - 1):
+                last = (x[i - 1], y[i - 1])
+                next = (x[i], y[i])
+
+                midpoint = ((last[1] + next[1]) / 2, (last[0] + next[0]) / 2)
+                normal = -(next[1] - last[1]) / (next[0] - last[0])
+                unit = (1, normal) / numpy.linalg.norm((1, normal))
+                
+                new_point = (midpoint[0] + unit[0] * self.length, midpoint[1] + unit[1] * self.length)
+                self.new_points.append(new_point)
+
+                plt.plot([midpoint[0], new_point[0]], [midpoint[1], new_point[1]], "b")
 
 
+if __name__ == "__main__":
+    WaypointGenerator("data/cloud_lasground.h5", [3400])
+    from show_gradient import site_slope_only
+    site_slope_only()
 
-from show_gradient import site_slope_only
-site_slope_only()
-
-plt.show()

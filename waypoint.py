900f572207738ac014d6295ac2f78f3a4de07473
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-06-24 11:27:10 -0400

    remove testing

--- waypoint.py
+++ waypoint.py
@@ -5,7 +5,6 @@ import numpy
 
 class WaypointGenerator:
     """Generate points fitted to a polynomial curve defined by points at a certain elevation, and offset it."""
-    # TODO: make the length work
     length = 150
 
     # this is just a random point in the highway
@@ -44,25 +43,18 @@ class WaypointGenerator:
                 normal = -(next[1] - last[1]) / (next[0] - last[0])
                 self.unit_normals.append(((1, normal) / numpy.linalg.norm((1, normal))).tolist())
 
-            # testing for now: modular length offset
-            self.normalize_middle()
+            # find the index of the unit normal with the minimum Y value, and stretch it to highway points
+            min_index = self.unit_normals.index(min(self.unit_normals, key=lambda x: abs(x[1])))
+            self.length = self.highway - self.midpoints[min_index][0]
+
+            # get new points
+            for (midpoint, unit) in zip(self.midpoints, self.unit_normals):
+                new_point = (midpoint[0] + unit[0] * self.length, midpoint[1] + unit[1] * self.length)
+                self.new_points.append(new_point)
 
             # graph the transformation for each points
             for (midpoint, new) in zip(self.midpoints, self.new_points):
                 plt.plot((midpoint[0], new[0]), (midpoint[1], new[1]), "b")
-
-    def constant_offset(self):
-        for (midpoint, unit) in zip(self.midpoints, self.unit_normals):
-            new_point = (midpoint[0] + unit[0] * self.length, midpoint[1] + unit[1] * self.length)
-            self.new_points.append(new_point)
-
-    def normalize_middle(self):
-        # find the index of the unit normal with the minimum Y value
-        min_index = self.unit_normals.index(min(self.unit_normals, key=lambda x: abs(x[1])))
-
-        # make the middle point stretch to highway point
-        self.length = self.highway - self.midpoints[min_index][0]
-        self.constant_offset()
 
 
 if __name__ == "__main__":

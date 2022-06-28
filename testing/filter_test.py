1aa013bc600b9581d56fcfdcae3d669125a99597
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-06-28 10:51:53 -0400

    Remove dependency in filter

--- testing/filter_test.py
+++ testing/filter_test.py
@@ -1,13 +1,34 @@
-from rectangular_gradient import InterpolatedGridGradient
 import numpy
+import pandas
+import scipy.interpolate
 import matplotlib.pyplot as plt
 
 
-def filter_slope(z_min, z_max):
-    ground = InterpolatedGridGradient("data/cloud_lasground.h5")
-    x, y = ground.x_grid, ground.y_grid
+def get_gradient():
+    # read in the las processed data
+    data = pandas.read_hdf("data/cloud_lasground.h5", "a").to_numpy()
+    spacing, values = data[..., :2], data[..., 2]
 
-    gradient = ground.magnitude
+    # get the x and y bounds and length
+    x_max, x_min = spacing[:, 0].max(), spacing[:, 0].min()
+    y_max, y_min = spacing[:, 1].max(), spacing[:, 1].min()
+    x_length, y_length = x_max - x_min, y_max - y_min
+
+    # create grid
+    x_grid = numpy.linspace(x_min, x_max, int(x_length * 2))
+    y_grid = numpy.linspace(y_min, y_max, int(y_length * 2))
+
+    # interpolate
+    points = scipy.interpolate.griddata(spacing, values, (x_grid[None, :], y_grid[:, None]), method="linear")
+
+    gradient = numpy.gradient(points, x_grid[1] - x_grid[0], y_grid[1] - y_grid[0])
+    magnitude = ((gradient[0] ** 2) + (gradient[1] ** 2)) ** 0.5
+
+    return x_grid, y_grid, magnitude, points
+
+
+def filter_slope(z_min, z_max):
+    x, y, gradient, points = get_gradient()
 
     gradient[(gradient < z_min) | (gradient > z_max)] = numpy.nan
 
@@ -18,11 +39,8 @@ def filter_slope(z_min, z_max):
 
 
 def filter_layer(z_filter):
-    ground = InterpolatedGridGradient("data/cloud_lasground.h5")
-    x, y = ground.x_grid, ground.y_grid
+    x, y, gradient, z = get_gradient()
     tol = 1
-
-    z = ground.points
 
     z[(z > z_filter + tol) | (z < z_filter - tol)] = numpy.nan
 
@@ -37,5 +55,3 @@ if __name__ == "__main__":
 
     # plt.imread("data/site.png")
     filter_layer(3600)
-
-

53cdfe3aa65e75bcb94bde075844dfb9f4d5eb4d
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-06-28 14:56:00 -0400

    Remove lots of code and fix variable naming

--- waypoint.py
+++ waypoint.py
@@ -26,60 +26,34 @@ class WaypointGenerator:
         # stash altitudes
         self.altitudes = height
 
-        for point in values:
+        for row in values:
             self.new_points = []
             self.points = []
             self.midpoints = []
             self.unit_normals = []
-            self.unit_normals_1 = []
 
-            point = numpy.array(sorted(point, key=lambda pt: pt[1]))
-            print(len(point))
-            # print(int((point[:, 1].max() - point[:, 1].min() + self.buf * 2) / 10))
-            print()
+            row = numpy.array(sorted(row, key=lambda pt: pt[1]))
 
             # polynomial fit
-            eq = numpy.polyfit(point[:, 1], point[:, 0], 3)
-            x = numpy.linspace(point[:, 1].min(), point[:, 1].max(), len(point))
+            eq = numpy.polyfit(row[:, 1], row[:, 0], 3)
 
             #TODO: Be able to add additional lines above the site to get additional coverage -- perhaps reuse a certain line (3600ft?) with a transformation
 
-            y = 0
-            for degree, coefficient in enumerate(eq[::-1]):
-                y += coefficient * x ** degree
+            for i in range(1, row.shape[0] - 1):
+                point = ((row[i - 1][0] + row[i][0]) / 2, (row[i - 1][1] + row[i][1]) / 2)
+                self.midpoints.append(point)
 
-            # Slope perpendicular to the fit
-            for i in range(1, len(x) - 1):
-                back = (x[i - 1], y[i - 1])
-                front = (x[i], y[i])
-
-                # get direction of normal vector
-                self.points.append(((back[1] + front[1]) / 2, (back[0] + front[0]) / 2))
-                normal = -(front[1] - back[1]) / (front[0] - back[0])
-                self.unit_normals.append(((1, normal) / numpy.linalg.norm((1, normal))).tolist())
-
-
-            for i in range(1, len(x) - 1):
-                self.midpoints.append(((point[i - 1][1] + point[i][1]) / 2, (point[i - 1][0] + point[i][0]) / 2))
-
-            for midpoint in self.midpoints:
-                # find unit normal at that point based on x value
-                x = midpoint[0]
-                y = 0
+                # find unit tangent at that point based on x value
+                y = point[1]
+                slope = 0
                 for degree, coefficient in enumerate(eq[::-1]):
-                    y += degree * coefficient * x ** (degree - 1)
-                self.unit_normals_1.append(((1, -y) / numpy.linalg.norm((1, y))).tolist())
-                
-                # get direction of normal vector
-                
-            # print(self.points)
-            # print()
-            # print(self.midpoints)
-            # input()
+                    slope += degree * coefficient * y ** (degree - 1)
 
+                self.unit_normals.append(((-slope, 1) / numpy.linalg.norm((-slope, 1))).tolist())
+                
             # get new points, stretching them by clearance off terrain
-            for (midpoint, unit) in zip(self.midpoints, self.unit_normals_1):
-                new_point = (midpoint[0] + unit[1] * self.clearance, midpoint[1] + unit[0] * self.clearance)
+            for (midpoint, normal) in zip(self.midpoints, self.unit_normals):
+                new_point = (midpoint[0] + normal[1] * self.clearance, midpoint[1] + normal[0] * self.clearance)
                 self.new_points.append(new_point)
 
             # reverse trajectory so altitude can increase on same side
@@ -87,16 +61,15 @@ class WaypointGenerator:
                 self.waypoints.append(self.new_points[::-1])
             else:
                 self.waypoints.append(self.new_points)
-
             self.inverse = not self.inverse
 
             if plot:
                 # plot original points and fit
-                plt.plot(point[:, 0], point[:, 1], "go")
+                plt.plot(row[:, 0], row[:, 1], "go")
 
                 # graph the transformation for each points
                 for (midpoint, new) in zip(self.midpoints, self.new_points):
-                    plt.plot((midpoint[1], new[1]), (midpoint[0], new[0]), "b")
+                    plt.plot((midpoint[0], new[0]), (midpoint[1], new[1]), "b")
 
         if plot:
             from show_gradient import site_slope_only
@@ -155,5 +128,5 @@ if __name__ == "__main__":
     # a.export()
 
     # WaypointGenerator("data/cloud_lasground.h5", [3400, 3450, 3500, 3550, 3600], plot=True)
-    WaypointGenerator("data/cloud_lasground.h5", [3500], plot=True)
-    # WaypointPlotter("data/cloud_lasground.h5", list(range(3400, 3600, 6)))
+    # WaypointGenerator("data/cloud_lasground.h5", [3500], plot=True)
+    WaypointPlotter("data/cloud_lasground.h5", list(range(3400, 3600, 6)))

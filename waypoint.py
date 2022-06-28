1b281d842916a3a9f6b3fc605524594a07549d07
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-06-28 14:31:11 -0400

    Make points go out of lines rather than fit

--- waypoint.py
+++ waypoint.py
@@ -15,7 +15,7 @@ class WaypointGenerator:
     waypoints: list = []
     altitudes: list = []
     new_points: list
-    midpoints: list
+    points: list
     unit_normals: list
     inverse: bool = False
 
@@ -28,15 +28,19 @@ class WaypointGenerator:
 
         for point in values:
             self.new_points = []
+            self.points = []
             self.midpoints = []
             self.unit_normals = []
+            self.unit_normals_1 = []
 
             point = numpy.array(sorted(point, key=lambda pt: pt[1]))
             print(len(point))
+            # print(int((point[:, 1].max() - point[:, 1].min() + self.buf * 2) / 10))
+            print()
 
             # polynomial fit
             eq = numpy.polyfit(point[:, 1], point[:, 0], 3)
-            x = numpy.linspace(point[:, 1].min() - self.buf, point[:, 1].max() + self.buf, int((point[:, 1].max() - point[:, 1].min() + self.buf * 2) / 10))
+            x = numpy.linspace(point[:, 1].min(), point[:, 1].max(), len(point))
 
             #TODO: Be able to add additional lines above the site to get additional coverage -- perhaps reuse a certain line (3600ft?) with a transformation
 
@@ -50,13 +54,32 @@ class WaypointGenerator:
                 front = (x[i], y[i])
 
                 # get direction of normal vector
-                self.midpoints.append(((back[1] + front[1]) / 2, (back[0] + front[0]) / 2))
+                self.points.append(((back[1] + front[1]) / 2, (back[0] + front[0]) / 2))
                 normal = -(front[1] - back[1]) / (front[0] - back[0])
                 self.unit_normals.append(((1, normal) / numpy.linalg.norm((1, normal))).tolist())
 
+
+            for i in range(1, len(x) - 1):
+                self.midpoints.append(((point[i - 1][1] + point[i][1]) / 2, (point[i - 1][0] + point[i][0]) / 2))
+
+            for midpoint in self.midpoints:
+                # find unit normal at that point based on x value
+                x = midpoint[0]
+                y = 0
+                for degree, coefficient in enumerate(eq[::-1]):
+                    y += degree * coefficient * x ** (degree - 1)
+                self.unit_normals_1.append(((1, -y) / numpy.linalg.norm((1, y))).tolist())
+                
+                # get direction of normal vector
+                
+            # print(self.points)
+            # print()
+            # print(self.midpoints)
+            # input()
+
             # get new points, stretching them by clearance off terrain
-            for (midpoint, unit) in zip(self.midpoints, self.unit_normals):
-                new_point = (midpoint[0] + unit[0] * self.clearance, midpoint[1] + unit[1] * self.clearance)
+            for (midpoint, unit) in zip(self.midpoints, self.unit_normals_1):
+                new_point = (midpoint[0] + unit[1] * self.clearance, midpoint[1] + unit[0] * self.clearance)
                 self.new_points.append(new_point)
 
             # reverse trajectory so altitude can increase on same side
@@ -70,11 +93,10 @@ class WaypointGenerator:
             if plot:
                 # plot original points and fit
                 plt.plot(point[:, 0], point[:, 1], "go")
-                plt.plot(y, x, "b")
 
                 # graph the transformation for each points
-                # for (midpoint, new) in zip(self.midpoints, self.new_points):
-                #     plt.plot((midpoint[0], new[0]), (midpoint[1], new[1]), "b")
+                for (midpoint, new) in zip(self.midpoints, self.new_points):
+                    plt.plot((midpoint[1], new[1]), (midpoint[0], new[0]), "b")
 
         if plot:
             from show_gradient import site_slope_only
@@ -132,5 +154,6 @@ if __name__ == "__main__":
     # a = WaypointGenerator("data/cloud_lasground.h5", [3400, 3450, 3500, 3550])
     # a.export()
 
-    WaypointGenerator("data/cloud_lasground.h5", [3400, 3450, 3500, 3550, 3600], plot=True)
+    # WaypointGenerator("data/cloud_lasground.h5", [3400, 3450, 3500, 3550, 3600], plot=True)
+    WaypointGenerator("data/cloud_lasground.h5", [3500], plot=True)
     # WaypointPlotter("data/cloud_lasground.h5", list(range(3400, 3600, 6)))

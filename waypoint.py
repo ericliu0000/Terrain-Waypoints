8c2a23f4ba429c6f8e53f6cc9c7f5cb3d004238e
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-07-11 11:43:55 -0400

    Fix order of normal vector

--- waypoint.py
+++ waypoint.py
@@ -14,15 +14,15 @@ class WaypointGenerator:
 
     def __init__(self, doc: str) -> None:
         site = SiteFilter(doc)
-        values = site.filtered
+        self.values = site.filtered
         inverted = False
 
-        for row in values:
+        for row in self.values:
             line = []
 
             # move every point normal by buf
             for point in row:
-                line.append([point[0] + point[3] * self.clearance, point[1] + point[4] * self.clearance, point[2] + point[5] * self.clearance])
+                line.append([point[0] + point[4] * self.clearance, point[1] + point[3] * self.clearance, point[2] + point[5] * self.clearance])
 
             # reverse every other line
             if inverted:
@@ -48,7 +48,7 @@ class WaypointGenerator:
             file.write("Latitude,Longitude,Altitude\n")
             for row in self.waypoints:
                 for point in row:
-                    x, y = p(point[0], point[1], inverse=True)    
+                    x, y = p(point[0], point[1], inverse=True)
                     file.write(f"{x},{y},{point[2]}\n")
 
 
@@ -63,12 +63,12 @@ class WaypointPlotter(WaypointGenerator):
         z = obj.height
 
         graph = plt.axes(projection="3d")
-        graph.set_xlabel("Easting")
-        graph.set_ylabel("Northing")
-        graph.set_zlabel("Altitude")
+        graph.set_xlabel("Easting (x)")
+        graph.set_ylabel("Northing (y)")
+        graph.set_zlabel("Altitude (z)")
 
-        x, y = numpy.meshgrid(x, y)
-        graph.plot_surface(x, y, z, linewidth=0, cmap=plt.cm.terrain)
+        # x, y = numpy.meshgrid(x, y)
+        # graph.plot_surface(x, y, z, linewidth=0, cmap=plt.cm.terrain)
 
         # Plot waypoints
         last = (self.waypoints[0][0][0], self.waypoints[0][0][1], self.waypoints[0][0][2])
@@ -77,6 +77,16 @@ class WaypointPlotter(WaypointGenerator):
                 plt.plot([point[0], last[0]], [point[1], last[1]], [point[2], last[2]], "r")
                 last = (point[0], point[1], point[2])
                 plt.plot(*last, "bo")
+
+        # opt 2: plot waypoints with line from original point to waypoint. note: remove inversion feature
+        # x_min = 950300
+        # y_min = 798900
+        # graph.set_xlim(x_min, x_min + 500)
+        # graph.set_ylim(y_min, y_min + 500)
+        # graph.set_zlim(3450, 3950)
+        # for r1, r2 in zip(self.values, self.waypoints):
+        #     for p1, p2 in zip(r1, r2):
+        #         plt.plot([p1[0], p2[0]], [p1[1], p2[1]], [p1[2], p2[2]], "r")
 
         plt.show()
 

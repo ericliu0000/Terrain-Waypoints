ceb945b69daaf7fdaa1f8e2a4be2e6ce9be8f301
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-08-30 15:41:08 -0400

    Fix ratio modifier

--- waypoint.py
+++ waypoint.py
@@ -11,19 +11,19 @@ from filter import SiteFilter
 class WaypointGenerator:
     waypoints: list = []
 
-    def __init__(self, doc: str, clearance=constants.CLEARANCE) -> None:
+    def __init__(self, doc: str, aclearance=constants.CLEARANCE) -> None:
         self.site = SiteFilter(doc)
         self.values = self.site.filtered
         inverted = False
-        self.clearance = clearance
+
 
         for row in self.values:
             line = []
 
             # Translate each point normal to the surface by clearance distance
             for point in row:
-                line.append([point[0] + point[3] * self.clearance, point[1] + point[4] * self.clearance,
-                             point[2] + point[5] * self.clearance])
+                line.append([point[0] + point[3] * aclearance, point[1] + point[4] * aclearance,
+                             point[2] + point[5] * constants.CLEARANCE])
 
             # Reverse the order of every other line
             if inverted:

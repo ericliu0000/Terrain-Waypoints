e3f4baa7b61fda3852a0b1baef9f1a4276645e60
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-09-06 15:47:32 -0400

    Add beginning of UI
    Make ui for generating waypoints; handlers for buttons and
    integration have not been completed

--- app.py
+++ app.py
@@ -1,30 +1,66 @@
+import pathlib
 import tkinter as tk
-import tkinter.font as tkFont
+
+import pygubu
+
+PROJECT_PATH = pathlib.Path(__file__).parent
+PROJECT_UI = PROJECT_PATH / "ui.ui"
+
+max_z = 3700
 
 
-class App:
-    def __init__(self, root):
-        # setting title
-        root.title("undefined")
-        # setting window size
-        width = 1280
-        height = 720
-        screenwidth = root.winfo_screenwidth()
-        screenheight = root.winfo_screenheight()
-        alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-        root.geometry(alignstr)
-        root.resizable(width=False, height=False)
+class Messages:
+    """Error messages."""
+    INVALID = ("#eb0b1f", "Inputs are not valid. Nothing was executed.")
+    NEGATIVE = ("#eb0b1f", "Inputs must be positive. Nothing was executed.")
+    OVERLAP = ("#eb0b1f", "Overlap value must be between 0 and 100%. Nothing was executed.")
+    Z_FILTER = ("#eb0b1f", f"Minimum Z height must be below {max_z} feet. Nothing was executed.")
+    LOW_MIN_HEIGHT = ("#ffa500", "Minimum distance is too low. Program executed, but results may be hazardous.")
+    OK_GRAPH = ("#000", "Graph generated successfully.")
+    OK_EXPORT_FT = ("#000", "Exporting to feet completed successfully to path: ")
+    OK_EXPORT_LL = ("#000", "Exporting to latitude/longitude completed successfully to path: ")
 
-        GLabel_822 = tk.Label(root)
-        # ft = tkFont.Font(family='Times',size=10)
-        # GLabel_822["font"] = ft
-        GLabel_822["fg"] = "#333333"
-        GLabel_822["justify"] = "center"
-        GLabel_822["text"] = "label"
-        GLabel_822.place(x=20, y=110, width=70, height=25)
+
+class UiApp:
+    count = 0
+    labels = []
+
+    def __init__(self, master=None, translator=None):
+        self.builder = builder = pygubu.Builder(translator)
+        builder.add_resource_path(PROJECT_PATH)
+        builder.add_from_file(PROJECT_UI)
+        # Main widget
+        self.mainwindow = builder.get_object("frame1", master)
+        builder.connect_callbacks(self)
+
+        for name, obj in self.builder.objects.items():
+            if type(obj) == pygubu.plugins.ttk.ttkstdwidgets.TTKEntry:
+                self.labels.append(name)
+
+    def run(self):
+        self.mainwindow.mainloop()
+
+    def handle(self, widget_id):
+        # Clear out previous error messages
+        self.builder.get_object("message").configure(text="")
+
+        try:
+            check = 0
+            for label in self.labels:
+                check += float(self.builder.get_object(label).get())
+                print(label)
+        except ValueError as e:
+            self.builder.get_object("message").configure(foreground="#eb0b1f", text="Inputs invalid, nothing was done")
+        # self.builder.get_object("message").configure(foreground="#000")
+        # self.builder.get_object("message").configure(text=str(self.count))
+        # Validate all inputs
+
+        print(widget_id)
+        print(self.builder.get_object("frame_h").get())
 
 
 if __name__ == "__main__":
     root = tk.Tk()
-    app = App(root)
-    root.mainloop()
+    root.geometry("800x600")
+    app = UiApp(root)
+    app.run()

8e6d386350d648887b837c28a8648f262b32f367
Eric Liu <55063842+ericliu0000@users.noreply.github.com>
2022-09-20 15:27:07 -0400

    Swap out parameters

--- app.py
+++ app.py
@@ -88,12 +88,14 @@ class UiApp:
             return
 
         # Transfer button info to variables and calculate frame spacing
-        frame_h, frame_v, overlap, dist, z_crop = labels["frame_h"], labels["frame_v"], labels["frame_overlap"], \
-                                                  labels["dist"], labels["z_crop"]
-        camera_h, camera_v = frame_h * (1 - overlap / 100), frame_v * (1 - overlap / 100)
+        frame_h, camera_h, camera_v, fov = labels["frame_h"], labels["cam_h"], labels["cam_v"], labels["fov"]
+        overlap, dist, z_crop = labels["frame_overlap"], labels["dist"], labels["z_crop"]
+
+        frame_v = frame_h * camera_v / camera_h
+        waypoint_h, waypoint_v = frame_h * (1 - overlap / 100), frame_v * (1 - overlap / 100)
 
         # Check for excessive run time and ask for confirmation
-        if (camera_h * camera_v) < 20:
+        if (waypoint_h * waypoint_v) < 20:
             if not self.long_run:
                 color, message = Messages.LONG_RUN_TIME
                 self.builder.get_object("message").configure(foreground=color, text=message)
@@ -103,8 +105,8 @@ class UiApp:
                 self.long_run = False
 
         # Set variables as configured
-        generate_waypoints.CAMERA_H = camera_h
-        generate_waypoints.CAMERA_V = camera_v
+        generate_waypoints.CAMERA_H = waypoint_h
+        generate_waypoints.CAMERA_V = waypoint_v
         generate_waypoints.CLEARANCE = dist
         generate_waypoints.Z_FILTER = z_crop
 
@@ -132,7 +134,7 @@ class UiApp:
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("800x600")
+    root.geometry("1000x600")
     root.title("Waypoint Generator")
     root.wm_iconphoto(False, tk.PhotoImage(file="data/site.png"))
     app = UiApp(root)
